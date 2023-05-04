atessmer/ibbqweb

commit 1087efc149dd5f5e648d432e8780c287f7e4ed19

Allow downloading chart data and viewing it back
Add the ability to download the chart data to a .json file, and then load those
saved data files back into the rendered chart.

When viewing saved chart data, the websocket to the server is disconnected and
a banner is shown to indicate that the data is not live, along with a button to
re-connect back to the server to view live data again.

webroot/assets/js/ibbq.js
@@ -1,10 +1,13 @@
 var ws;
 var serverDisconnectedBanner;
+var offlineModeBanner;
 var ibbqConnection;
 var ibbqBattery;
 var ibbqUnitCelcius;
 var chart;
 var tempAlertModal;
+
+var inOfflineMode = false;
 var inSilenceAlarmHandler = false;
 
 var alertAudio = new Audio('/assets/audio/AlertTone.mp3');
@@ -329,27 +332,51 @@ function renderChart(minRenderIntervalMs=50) {
 }
 document.addEventListener("visibilitychange", renderChart);
 
+function resetChartData() {
+   chart.options.data = []
+   chart.options.axisY.stripLines = []
+   var xMin = new Date().getTime()
+   for (var i = 0; i < data.probe_readings.length; i++) {
+      var reading = data.probe_readings[i]
+      if (reading.probes.some(temp => temp != null)) {
+         xMin = reading.ts
+         break
+      }
+   }
+   chart.options.axisX.minimum = xMin
+   chart.options.axisX.maximum = xMin + (10 * 60 * 1000) // +10 min
+}
+
 function connectWebsocket() {
+   if (inOfflineMode) {
+      return
+   }
+
    protocol = window.location.protocol == "https:" ? "wss://" : "ws://"
    ws = new WebSocket(protocol + window.location.host + "/ws")
 
    ws.onopen = function(event) {
-      if (serverDisconnectedBanner.classList.contains("show")) {
+      if (serverDisconnectedBanner.classList.contains("show") || offlineModeBanner.classList.contains("show")) {
          console.log("websocket opened")
          serverDisconnectedBanner.classList.remove("show")
+         offlineModeBanner.classList.remove("show")
          renderChart()
       }
    }
 
    ws.onclose = function(event) {
-      if (!serverDisconnectedBanner.classList.contains("show")) {
+      ibbqConnection.classList.remove("connected")
+      ibbqConnection.classList.remove("disconnected")
+      if (inOfflineMode) {
+         serverDisconnectedBanner.classList.remove("show")
+         offlineModeBanner.classList.add("show")
+      } else if (!serverDisconnectedBanner.classList.contains("show")) {
          console.warn("websocket closed: [" + event.code + "]")
          serverDisconnectedBanner.classList.add("show")
-         ibbqConnection.classList.remove("connected")
-         ibbqConnection.classList.remove("disconnected")
+         offlineModeBanner.classList.remove("show")
+         setTimeout(connectWebsocket, 1000)
          renderChart()
       }
-      setTimeout(connectWebsocket, 1000)
    }
 
    ws.onmessage = function (event) {
@@ -399,19 +426,7 @@ function connectWebsocket() {
           * Update probe data (probe and chart tabs)
           */
          if (data.full_history) {
-            // Reset chart
-            chart.options.data = [];
-            chart.options.axisY.stripLines = [];
-            var xMin = new Date().getTime();
-            for (var i = 0; i < data.probe_readings.length; i++) {
-               var reading = data.probe_readings[i];
-               if (reading.probes.some(temp => temp != null)) {
-                  xMin = reading.ts;
-                  break;
-               }
-            }
-            chart.options.axisX.minimum = xMin;
-            chart.options.axisX.maximum = xMin + (10 * 60 * 1000); // +10 min
+            resetChartData()
          }
 
          if (data.full_history || data.connected) {
@@ -475,6 +490,7 @@ function connectWebsocket() {
 document.onreadystatechange = function() {
    if (document.readyState === "complete") {
       serverDisconnectedBanner = document.getElementById("server-disconnected-banner");
+      offlineModeBanner = document.getElementById("offline-mode-banner");
       ibbqConnection = document.querySelector("#ibbq-connection");
       ibbqBattery = document.querySelector("#ibbq-battery");
       ibbqUnitCelcius = document.getElementById("ibbq-unit-celcius");
@@ -492,6 +508,63 @@ document.onreadystatechange = function() {
             ws.send(JSON.stringify({
                cmd: 'clear_history',
             }))
+         }
+      )
+
+      document.getElementById("ibbq-download").addEventListener(
+         'click', function(event) {
+            probe_readings = new Map()
+            for (var i = 0; i < chart.options.data.length; i++) {
+               var dataSeries = chart.options.data[i];
+               for (var j = 0; j < dataSeries.dataPoints.length; j++) {
+                  var dataPoint = dataSeries.dataPoints[j]
+
+                  if (probe_readings.get(dataPoint.x) == undefined) {
+                     probe_readings.set(dataPoint.x, [])
+                  }
+                  probe_readings.get(dataPoint.x)[i] = dataPoint.tempC
+               }
+            }
+
+            data = {
+               'probe_readings': Array.from(probe_readings.keys()).reduce((result, ts) => {
+                  result.push({
+                     'ts': ts,
+                     'probes': probe_readings.get(ts)
+                  })
+                  return result
+                }, []),
+            }
+
+            blob = new Blob([JSON.stringify(data)], {type: 'application/json'}) // text/plain
+            this.href = window.URL.createObjectURL(blob)
+            this.download = 'ibbq_' + new Date().toJSON().slice(0, -5) + '.json'
+         }
+      )
+
+      document.getElementById("ibbq-upload").addEventListener(
+         'change', function(e) {
+            var reader = new FileReader()
+            reader.readAsText(e.target.files[0], 'UTF-8')
+            reader.onload = e2 => {
+               data = JSON.parse(e2.target.result);
+
+               // Disconnect from server
+               inOfflineMode = true
+               ws.close()
+
+               resetChartData()
+               for (var i = 0; i < data.probe_readings.length; i++) {
+                  appendChartData(data.probe_readings[i]);
+               }
+            }
+         }
+      )
+
+      document.getElementById('server-reconnect').addEventListener(
+         'click', function(event) {
+            inOfflineMode = false
+            connectWebsocket()
          }
       )
 

webroot/assets/css/styles.css
@@ -6,13 +6,11 @@ body {
   height: 100%;
 }
 
-#server-disconnected-banner {
-  background-color: red;
-  color: #fff;
+.banner {
   text-align: center;
   height: 0;
 }
-#server-disconnected-banner.show {
+.banner.show {
   height: auto;
 }
 
@@ -112,7 +110,7 @@ body {
 /*
  * Battery Indicator
  */
-.bi::before {
+#ibbq-battery::before {
   transform: rotate(270deg);
 }
 

webroot/index.html
@@ -17,8 +17,14 @@
   </head>
   <body>
     <div class="container-fluid h-100 p-0 d-flex flex-column">
-      <div id="server-disconnected-banner" class="flex-shrink-0">
+      <div id="server-disconnected-banner" class="flex-shrink-0 banner bg-danger text-light">
         <div class="p-1">Reconnecting to Server...</div>
+      </div>
+      <div id="offline-mode-banner" class="flex-shrink-0 banner bg-warning text-secondary">
+        <div class="p-1">
+          Disconnected from server while viewing saved data.
+          <a id="server-reconnect" href="#" class="btn btn-sm btn-secondary py-0">Reconnect</a>
+        </div>
       </div>
       <nav class="navbar navbar-expand navbar-dark bg-dark pb-0 mb-2 flex-shrink-0">
         <div class="container-fluid">
@@ -71,10 +77,25 @@
               <input id="chart-min-y" class="form-control" autocomplete="off" value="0" type="number" />
             </div>
           </div>
-          <div class="row mb-3 mt-5">
-            <label class="col-6 col-md-4 col-xl-2 col-form-label">&nbsp;</label>
+          <div class="row mb-3">
+            <label class="col-6 col-md-4 col-xl-2 col-form-label">Save Chart Data</label>
             <div class="col-6 col-md-4 col-xl-2">
-              <input id="ibbq-clear-history" class="btn btn-sm btn-danger w-100" type="button" value="Clear Temperature History">
+              <a id="ibbq-download" class="btn btn-sm btn-secondary w-100" href="#"><i class="bi bi-download"></i> Download</a>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-6 col-md-4 col-xl-2 col-form-label">View Saved Data</label>
+            <div class="col-6 col-md-4 col-xl-2">
+              <div class="file btn btn-sm btn-secondary w-100 position-relative overflow-hidden">
+                <i class="bi bi-upload"></i> Upload
+                <input type="file" id="ibbq-upload" class="w-100 position-absolute top-0 start-0 opacity-0">
+              </div>
+            </div>
+          </div>
+          <div class="row mb-3 mt-5">
+            <label class="col-6 col-md-4 col-xl-2 col-form-label">Clear Data</label>
+            <div class="col-6 col-md-4 col-xl-2">
+              <a id="ibbq-clear-history" class="btn btn-sm btn-danger w-100"><i class="bi bi-trash-fill"></i> Clear</a>
             </div>
           </div>
         </div>

commit 64cdaf187f77d41cb373c78976b0bdd638de5859

Handle repeated connection failures
While the developer console shows connection errors (ex: timeout) as happening
on the line creating the websocket object, they cannot be caught with a
try/catch around the websocket creation as the error happens asynchronously;
thus the try/catch is removed.

Instead, both the onclose() and onerror() callbacks are issued when such a
connection error occurs. The issue is actually a logic bug introduced in
1087efc1 when offlineMode was added. When the onclose() callback is issued,
unless we're in offline mode we need to always create a timeout to call
connectWebsocket() and try to re-connect... not just the first time (i.e. when
the banner isn't showing yet). This ensures we will continue to try to
reconnect the websocket until it succeeds.

webroot/assets/js/ibbq.js
@@ -230,12 +230,7 @@ connectWebsocket = () => {
    }
 
    protocol = window.location.protocol == "https:" ? "wss://" : "ws://"
-   try {
-      ws = new WebSocket(protocol + window.location.host + "/ws")
-   } catch (e) {
-      setTimeout(connectWebsocket, 1000)
-      return
-   }
+   ws = new WebSocket(protocol + window.location.host + "/ws")
 
    ws.onopen = (e) => {
       if (!serverDisconnectedBanner.classList.contains("d-none") || !offlineModeBanner.classList.contains("d-none")) {
@@ -252,13 +247,16 @@ connectWebsocket = () => {
       if (inOfflineMode) {
          serverDisconnectedBanner.classList.add("d-none")
          offlineModeBanner.classList.remove("d-none")
-      } else if (serverDisconnectedBanner.classList.contains("d-none")) {
+         return
+      }
+
+      if (serverDisconnectedBanner.classList.contains("d-none")) {
          console.warn("websocket closed: [" + e.code + "]")
          serverDisconnectedBanner.classList.remove("d-none")
          offlineModeBanner.classList.add("d-none")
-         setTimeout(connectWebsocket, 1000)
          renderChart()
       }
+      setTimeout(connectWebsocket, 1000)
    }
 
    ws.onmessage = (e) => {