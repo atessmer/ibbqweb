6944c94111714d60b83eb597372112bf27d6c8b7
Alex Tessmer <alex@tessmer.dev>
2024-07-05 19:44:57 -0500

    Rewrite cookie handling in modern javascript
    Make the code more readable by using verbose variable names, standard
    string methods to split/trim, remove global 'var' usage, etc.

--- webroot/assets/js/ibbq.js
+++ webroot/assets/js/ibbq.js
@@ -43,28 +43,28 @@ const tempToC = (temp) => temp != null && isUnitF() ? FtoC(temp) : temp
  * source: https://www.quirksmode.org/js/cookies.html
  */
 const createCookie = (name, value, days) => {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+   let expires = "";
+   if (days) {
+      const date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      expires = "; expires="+date.toGMTString();
+   }
+   document.cookie = name+"="+value+expires+"; path=/";
 }
 
 const readCookie = (name) => {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+   const cookies = document.cookie.split(";");
+   for (const cookie of cookies) {
+      [cookie_name, cookie_value] = cookie.trim().split("=");
+      if (cookie_name == name) {
+         return cookie_value;
+      }
+   }
+   return null;
 }
 
 const eraseCookie = (name) => {
-	createCookie(name,"",-1);
+   createCookie(name, "", -1);
 }
 
 // TODO: support Celcius presets too
